1a3abc95d432da316b35a1b223e983d598003274
Tom Turrell-Croft <tom@berrycloud.co.uk>
2014-10-31 22:42:50 +0000

    added bug fix to DNS

--- active-directory/active-directory-federation-services/Configure-Network.ps1
+++ active-directory/active-directory-federation-services/Configure-Network.ps1
@@ -1,5 +1,19 @@
+param(
+    [string]$dns1 = "Dns1", 
+    [string]$dns2 = "Dns2"
+    )
+
 $netip = Get-NetIPConfiguration
 $ipconfig = Get-NetIPAddress | ?{$_.IpAddress -eq $netip.IPv4Address.IpAddress}
 Get-NetAdapter | Set-NetIPInterface -DHCP Disabled
 Get-NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $netip.IPv4Address.IpAddress -PrefixLength $ipconfig.PrefixLength -DefaultGateway $netip.IPv4DefaultGateway.NextHop
-Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $netip.DNSServer.ServerAddresses
+
+
+if(![string]::IsNullOrEmpty($dns1))
+{
+	Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $dns1 $dns2
+}
+else
+{
+	Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $netip.DNSServer.ServerAddresses
+}
