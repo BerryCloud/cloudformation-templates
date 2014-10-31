0da861ea881848bee5dc84122ffb5e15db350d3d
Tom Turrell-Croft <tom@berrycloud.co.uk>
2014-10-31 17:22:19 +0000

    Added network configutation script

---
+++ active-directory/active-directory-federation-services/Configure-Network.ps1
@@ -0,0 +1,5 @@
+$netip = Get-NetIPConfiguration
+$ipconfig = Get-NetIPAddress | ?{$_.IpAddress -eq $netip.IPv4Address.IpAddress}
+Get-NetAdapter | Set-NetIPInterface -DHCP Disabled
+Get-NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $netip.IPv4Address.IpAddress -PrefixLength $ipconfig.PrefixLength -DefaultGateway $netip.IPv4DefaultGateway.NextHop
+Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $netip.DNSServer.ServerAddresses
