76c01ec5d5530294632c2985c6690f3392aec888
Tom Turrell-Croft <tom@berrycloud.co.uk>
2014-11-08 22:28:26 +0000

    woring version

--- active-directory/active-directory-federation-services/Configure-ADDS.ps1
+++ active-directory/active-directory-federation-services/Configure-ADDS.ps1
@@ -5,3 +5,4 @@ param(
     )
 
 Install-ADDSForest -DomainMode 6 -DomainName $domainName -DomainNetbiosName $domainNetbiosName -ForestMode 6 -SafeModeAdministratorPassword (convertto-securestring $safeModeAdministratorPassword -asplaintext -force) -Confirm:$false
+powershell.exe -Command Restart-Service NetLogon -EA 0
--- active-directory/active-directory-federation-services/Configure-Network.ps1
+++ active-directory/active-directory-federation-services/Configure-Network.ps1
@@ -8,11 +8,12 @@ $ipconfig = Get-NetIPAddress | ?{$_.IpAddress -eq $netip.IPv4Address.IpAddress}
 Get-NetAdapter | Set-NetIPInterface -DHCP Disabled
 Get-NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $netip.IPv4Address.IpAddress -PrefixLength $ipconfig.PrefixLength -DefaultGateway $netip.IPv4DefaultGateway.NextHop
 
-if(![string]::IsNullOrEmpty($dns1) -and [string]::IsNullOrEmpty($dns2))
+if(($dns1 -notmatch "Dns1") -and ($dns2 -match "Dns2"))
 {
 	Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses "$dns1"
+
 }
-elseif(![string]::IsNullOrEmpty($dns1) -and ![string]::IsNullOrEmpty($dns2))
+elseif(($dns1 -notmatch "Dns1") -and ($dns2 -notmatch "Dns2"))
 {
     Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses "$dns1","$dns2"
 }
