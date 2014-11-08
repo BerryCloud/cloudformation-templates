bafd1dc790d7ad9b34e99fe205a1a810f1252578
Tom Turrell-Croft <tom@berrycloud.co.uk>
2014-11-08 00:07:39 +0000

    fixed bugs

--- active-directory/active-directory-federation-services/Configure-Network.ps1
+++ active-directory/active-directory-federation-services/Configure-Network.ps1
@@ -8,10 +8,13 @@ $ipconfig = Get-NetIPAddress | ?{$_.IpAddress -eq $netip.IPv4Address.IpAddress}
 Get-NetAdapter | Set-NetIPInterface -DHCP Disabled
 Get-NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $netip.IPv4Address.IpAddress -PrefixLength $ipconfig.PrefixLength -DefaultGateway $netip.IPv4DefaultGateway.NextHop
 
-
-if(![string]::IsNullOrEmpty($dns1))
+if(![string]::IsNullOrEmpty($dns1) -and [string]::IsNullOrEmpty($dns2))
 {
-	Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $dns1 $dns2
+	Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses "$dns1"
+}
+elseif(![string]::IsNullOrEmpty($dns1) -and ![string]::IsNullOrEmpty($dns2))
+{
+    Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses "$dns1","$dns2"
 }
 else
 {
